00de0833d7f57159b916a8ce37c1a5965c745cf3
Tomassandi <t.sandi@hotmail.com>
2020-10-07 21:00:58 -0600

    entrega tarea

---
+++ src/variableexample.java
@@ -0,0 +1,12 @@
+public class variableexample {
+
+    public static void main(String[] args) {
+        short variableNumerica16bits=Short.MAX_VALUE;
+
+        long variablenumerica64bits=Integer.MAX_VALUE+1;
+
+        double variablenoenterabits=23.5;
+    }
+
+}
+
